54d2698201691ac864cd01f9111fa63a21569e99
Peter Kurzok <peter.kurzok@maibornwolff.de>
2021-01-14 12:15:53 +0100

    Added AlamoFire Service including Test

--- Networking/SampleService.swift
+++ Networking/SampleService.swift
@@ -5,6 +5,7 @@
 //  Created by Peter Kurzok on 13.01.21.
 //
 
+import Alamofire
 import Foundation
 
 protocol SampleServiceProtocol {
@@ -29,18 +30,30 @@ struct SampleService: SampleServiceProtocol {
     private func handleResponseData(data: Data?, completion: @escaping (_ results: [SampleModel]) -> Void) {
         guard let data = data else { completion([SampleModel]()); return }
 
-        if let jsonString = String(data: data, encoding: .utf8) {
-            print(jsonString)
-        } else {
-            print("No data")
-        }
-
         do {
             let modelData = try JSONDecoder().decode([SampleModel].self, from: data)
             completion(modelData)
         } catch {
             print(error.localizedDescription)
             completion([SampleModel]())
+        }
+    }
+}
+
+struct AFService: SampleServiceProtocol {
+    var session: Session
+    
+    init(session: Session = AF) {
+        self.session = session
+    }
+    
+    func getSomething(completion: @escaping ([SampleModel]) -> Void) {
+        session.request("https://jsonplaceholder.typicode.com/users").responseDecodable(of: [SampleModel].self) {
+            response in
+    
+            guard let resultList = response.value else { completion([SampleModel]()); return }
+            
+            completion(resultList)
         }
     }
 }
--- NetworkingTests/NetworkingTests.swift
+++ NetworkingTests/NetworkingTests.swift
@@ -5,6 +5,7 @@
 //  Created by Peter Kurzok on 13.01.21.
 //
 
+import Alamofire
 @testable import Networking
 import XCTest
 
@@ -44,6 +45,29 @@ class NetworkingTests: XCTestCase {
         let expectation = self.expectation(description: "AsyncCall")
         
         let sampleService = SampleService(urlSession: session)
+        sampleService.getSomething { results in
+            
+            XCTAssertNotNil(results)
+            XCTAssertEqual(10, results.count)
+            
+            XCTAssertEqual("Leanne Graham", results.first?.name)
+            XCTAssertEqual("Rey.Padberg@karina.biz", results.last?.email)
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testAfLogging() {
+        let config = URLSessionConfiguration.default
+        config.protocolClasses?.insert(LoggingProtocol.self, at: 0)
+        
+        let session = Session(configuration: config)
+        
+        let expectation = self.expectation(description: "AsyncCall")
+        
+        let sampleService = AFService(session: session)
         sampleService.getSomething { results in
             
             XCTAssertNotNil(results)
